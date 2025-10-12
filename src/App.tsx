c584d79217445bb8410d3722ea41266f1997a133
livebylee <livebylee@gmail.com>
2025-10-12 21:54:05 +0900

    remove white space

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Hello Im lee</h1>
-      <h1> final test</h1>
+      <h1>final test</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
