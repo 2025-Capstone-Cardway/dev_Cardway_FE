05b0ad200eadcd19f52311b74213e285f93ac651
livebylee <livebylee@gmail.com>
2025-11-11 00:23:41 +0900

    fix : login path

--- src/components/login/KakaoLoginButton.tsx
+++ src/components/login/KakaoLoginButton.tsx
@@ -1,21 +1,9 @@
-import axios from 'axios';
 import kakao_login from "../../assets/login/kakao_login.png";
 
 
 const KakaoLoginButton = () => {
-    const API_BASE_URL = "http://localhost:8080";
-    
-    const handleLogin = async () => {
-
-        try{
-            const response = await axios.get(`${API_BASE_URL}/api/auth/kakao/login`);
-
-            const {redirectUrl} = response.data;
-
-            window.location.href = redirectUrl;
-        } catch (error){
-            console.error("로그인 요청 실패: ",error);
-        }
+    const handleLogin = () => {
+        window.location.href = "https://streaky-ned-noncontrollablely.ngrok-free.dev/oauth2/authorization/kakao";
     };
     return (
         <button onClick={handleLogin} className="flex items-center justify-center h-12 rounded-lg bg-[#FEE500] hover:bg-[#FDD835] transition">
--- src/components/login/NaverLoginButton.tsx
+++ src/components/login/NaverLoginButton.tsx
@@ -1,21 +1,9 @@
-import axios from 'axios';
 import naver_login from "../../assets/login/naver_login.png";
 
 
 const NaverLoginButton = () => {
-    const API_BASE_URL = "http://localhost:8080";
-    
-    const handleLogin = async () => {
-
-        try{
-            const response = await axios.get(`${API_BASE_URL}/api/auth/kakao/login`);
-
-            const {redirectUrl} = response.data;
-
-            window.location.href = redirectUrl;
-        } catch (error){
-            console.error("로그인 요청 실패: ",error);
-        }
+    const handleLogin = () => {
+        window.location.href = "https://streaky-ned-noncontrollablely.ngrok-free.dev/oauth2/authorization/naver";
     };
     return (
         <button onClick={handleLogin} className="flex items-center justify-center h-12 rounded-lg bg-[#03C75A] hover:brightness-110 transition">
