39400f4eec75d376ae8b9e07efa19e6e78ae57e4
livebylee <livebylee@gmail.com>
2025-11-11 00:33:30 +0900

    set : vercel setting

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,7 +4,7 @@ import NaverLoginButton from "../components/login/NaverLoginButton";
 
 export default function LoginPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+    <div className="flex flex-col items-center justify-end min-h-screen bg-gray-50 pb-20">
       <div className="flex flex-col gap-3 w-full max-w-[320px]">
         <KakaoLoginButton />
         <NaverLoginButton />
